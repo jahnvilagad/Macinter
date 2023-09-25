e404110dfeef2e8b074717ffcc5272b9cabfab03
viralsolanki <viralsolanki2@gmail.com>
2023-09-25 14:19:24 +0530

    first commit

---
+++ assets/js/custom.js
@@ -0,0 +1,11 @@
+(function () {
+    window.onload = function () {
+      var preloader = document.querySelector('.page-loading');
+      preloader.classList.remove('active');
+      setTimeout(function () {
+        preloader.remove();
+      }, 2000);
+    };
+  })();
+
+  
