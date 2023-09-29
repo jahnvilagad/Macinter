6714bdcf6f6778c1fe774bd26e2899e34d0a91ff
viralsolanki <viralsolanki2@gmail.com>
2023-09-29 15:32:22 +0530

    create pages

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -71,46 +71,46 @@
   });
 
 
-  $(document).ready(function () {
-    $('.carousel-slick-testimonial').slick({
-      speed: 500,
-      slidesToShow: 2,
-      slidesToScroll: 1,
-      autoplay: true,
-      autoplaySpeed: 4000,
-      dots: true,
-      centerMode: true,
-      responsive: [{
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          // centerMode: true,
+  // $(document).ready(function () {
+  //   $('.carousel-slick-testimonial').slick({
+  //     speed: 500,
+  //     slidesToShow: 2,
+  //     slidesToScroll: 1,
+  //     autoplay: true,
+  //     autoplaySpeed: 4000,
+  //     dots: true,
+  //     centerMode: true,
+  //     responsive: [{
+  //       breakpoint: 1024,
+  //       settings: {
+  //         slidesToShow: 1,
+  //         slidesToScroll: 1,
+  //         // centerMode: true,
 
-        }
+  //       }
 
-      }, {
-        breakpoint: 800,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 2,
-          dots: true,
-          infinite: true,
+  //     }, {
+  //       breakpoint: 800,
+  //       settings: {
+  //         slidesToShow: 1,
+  //         slidesToScroll: 2,
+  //         dots: true,
+  //         infinite: true,
 
-        }
-      }, {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          dots: true,
-          infinite: true,
-          autoplay: true,
-          autoplaySpeed: 4000,
-        }
-      }]
-    });
-  });
+  //       }
+  //     }, {
+  //       breakpoint: 480,
+  //       settings: {
+  //         slidesToShow: 1,
+  //         slidesToScroll: 1,
+  //         dots: true,
+  //         infinite: true,
+  //         autoplay: true,
+  //         autoplaySpeed: 4000,
+  //       }
+  //     }]
+  //   });
+  // });
 
 
   $(document).ready(function () {
@@ -172,4 +172,45 @@
 
 
     // document ready  
+  });
+
+  $(document).ready(function(){
+    $('.carousel-slick-testimonial').slick({
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 4000,
+      dots:true,
+      centerMode: true,
+      responsive: [{
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          // centerMode: true,
+  
+        }
+  
+      }, {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 2,
+          dots: true,
+          infinite: true,
+  
+        }
+      },  {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: true,
+          infinite: true,
+          autoplay: true,
+          autoplaySpeed: 4000,
+        }
+      }]
+    });
   });
