4e4260de8ed23652c85c4193654933b9283b4c2b
jahnvi <jahnvi@thewebsitefactory.london>
2023-10-09 20:48:31 +0530

    changes

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -214,3 +214,24 @@
       }]
     });
   });
+
+
+  $(function() {
+    'use strict';
+
+
+    $(activate);
+
+
+    function activate() {
+
+    $('.dashboard .nav-tabs')
+        .scrollingTabs({
+        enableSwiping: true
+        })
+        .on('ready.scrtabs', function() {
+        $('.tab-content').show();
+        });
+
+    }
+}());
