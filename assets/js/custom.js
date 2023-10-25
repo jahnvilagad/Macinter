920dd8b524af0dce04b2332bbb7f59bcc10443c2
viralsolanki <viralsolanki2@gmail.com>
2023-10-25 15:26:16 +0530

    new changes

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -71,48 +71,6 @@
   });
 
 
-  // $(document).ready(function () {
-  //   $('.carousel-slick-testimonial').slick({
-  //     speed: 500,
-  //     slidesToShow: 2,
-  //     slidesToScroll: 1,
-  //     autoplay: true,
-  //     autoplaySpeed: 4000,
-  //     dots: true,
-  //     centerMode: true,
-  //     responsive: [{
-  //       breakpoint: 1024,
-  //       settings: {
-  //         slidesToShow: 1,
-  //         slidesToScroll: 1,
-  //         // centerMode: true,
-
-  //       }
-
-  //     }, {
-  //       breakpoint: 800,
-  //       settings: {
-  //         slidesToShow: 1,
-  //         slidesToScroll: 2,
-  //         dots: true,
-  //         infinite: true,
-
-  //       }
-  //     }, {
-  //       breakpoint: 480,
-  //       settings: {
-  //         slidesToShow: 1,
-  //         slidesToScroll: 1,
-  //         dots: true,
-  //         infinite: true,
-  //         autoplay: true,
-  //         autoplaySpeed: 4000,
-  //       }
-  //     }]
-  //   });
-  // });
-
-
   $(document).ready(function () {
     $(".customer-logos").slick({
       slidesToShow: 6,
@@ -139,40 +97,6 @@
     });
   });
 
-  $(document).ready(function () {
-
-    // Gets the video src from the data-src on each button
-
-    var $videoSrc;
-    $('.video-btn').click(function () {
-      $videoSrc = $(this).data("src");
-    });
-    console.log($videoSrc);
-
-
-
-    // when the modal is opened autoplay it  
-    $('#myModal').on('shown.bs.modal', function (e) {
-
-      // set the video src to autoplay and not to show related video. Youtube related video is like a box of chocolates... you never know what you're gonna get
-      $("#video").attr('src', $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
-    })
-
-
-
-    // stop playing the youtube video when I close the modal
-    $('#myModal').on('hide.bs.modal', function (e) {
-      // a poor man's stop video
-      $("#video").attr('src', $videoSrc);
-    })
-
-
-
-
-
-
-    // document ready  
-  });
 
   $(document).ready(function(){
     $('.carousel-slick-testimonial').slick({
@@ -221,26 +145,6 @@
       $(".mac-indicator").addClass("d-none");
     });
   });
-
-//   $(function() {
-//     'use strict';
-
-
-//     $(activate);
-
-
-//     function activate() {
-
-//     $('.dashboard .nav-tabs')
-//         .scrollingTabs({
-//         enableSwiping: true
-//         })
-//         .on('ready.scrtabs', function() {
-//         $('.tab-content').show();
-//         });
-
-//     }
-// }());
 
 $(function() {
   'use strict';
